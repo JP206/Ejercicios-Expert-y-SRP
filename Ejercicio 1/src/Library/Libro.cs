8a30869bf7e1fb1d81f0d62862bf5887ef48f8aa
[Sofia Pareja] <[sofia.pareja@correo.ucu.edu.uy]>
2021-09-01 09:55:39 -0300

    Comentarios

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -8,8 +8,8 @@ namespace SRP
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
-        public string EstanteBiblioteca { get ; set; }
+        public string SectorBiblioteca { get ; set; } // Deberia pertenecer a otra clase Biblioteca
+        public string EstanteBiblioteca { get ; set; } // Deberia pertenecer a otra clase Biblioteca 
 
         public Libro(String titulo, String autor, String codigo)
         {
@@ -17,7 +17,8 @@ namespace SRP
             this.Autor = autor;
             this.Codigo = codigo;
         }
-
+        //  El metodo deberia pertencer a la clase Biblioteca
+        // La clase Libro seria un colaborador de la clase Biblioteca
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
